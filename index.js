0bb0943e76dd9d5b14973aa70f1e8cde52ab4a9c
blar khalid <blarharki@gmail.com>
2018-09-04 08:04:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,10 @@
 var customerName='bob';
 function upperCaseCustomerName(){
   customerName=customerName.toUpperCase()
-};
+}
 function setBestCustomer(){
   bestCustomer='not bob'
+}
+function overwriteBestCustomer(){
+  
 }
